7f8203e9ebe57ca4ae942321f6a8128acc57dd02
Naapppssss <c.napoleone97@gmail.com>
2022-03-01 17:06:59 -0300

    Ejercicio1

---
+++ ejercicio1/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int numero1 = 0;
+        System.out.println(numero1);
+
+        numero1 += 15;
+        System.out.println(numero1);
+
+        int miArray [];
+        miArray = new int [4];
+        miArray = new int [] {1, 2, 3, 4};
+
+        System.out.println(miArray);
+    }
+}
