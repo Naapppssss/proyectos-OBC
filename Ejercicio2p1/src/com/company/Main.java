5338d8c0ee7b6daae829e6b8da60175a6d07cc69
Naapppssss <c.napoleone97@gmail.com>
2022-03-03 01:07:22 -0300

    ejercicio2p1

---
+++ Ejercicio2p1/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int resultado;
+        suma( 4, 2, 6 );
+
+    }
+    public static void suma(int a, int b, int c) {
+        int resultado;
+        resultado = a + b + c ;
+        System.out.println(resultado);
+    }
+}
