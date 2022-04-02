6138f2dfdc46080f405d190e44c879ecb6347de5
Naapppssss <c.napoleone97@gmail.com>
2022-04-02 20:50:02 -0300

    While

---
+++ Sesion4/p2/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int numeroWhile = 0;
+        while  (numeroWhile < 3) {
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1 ;
+        }
+    }
+}
