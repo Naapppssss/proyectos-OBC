0f8b9f6eadf7da4e8b94a2afea3c74837a662bfd
Naapppssss <c.napoleone97@gmail.com>
2022-04-01 23:57:37 -0300

    p1 if

---
+++ tema4/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+         int numeroIf = 0;
+
+         if ( numeroIf == 0 ) {
+             System.out.println( 0 );
+         } else if ( numeroIf >= 0 ) {
+             System.out.println("positivo");
+         } else {
+             System.out.println( "negativo");
+        }
+    }
+}
