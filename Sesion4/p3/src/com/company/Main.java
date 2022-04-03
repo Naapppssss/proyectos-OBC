660eaefc00ba726ffe49a1ee625d3a9c502347e1
Naapppssss <c.napoleone97@gmail.com>
2022-04-02 21:10:09 -0300

    DoWhile

---
+++ Sesion4/p3/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int numeroWhile = 2;
+        do {
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1;
+        } while  (numeroWhile < 3);
+    }
+}
