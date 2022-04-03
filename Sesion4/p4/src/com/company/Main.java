cd107bef7f92b692a17077983104b8517aeb2964
Naapppssss <c.napoleone97@gmail.com>
2022-04-02 21:14:54 -0300

    For

---
+++ Sesion4/p4/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int numeroFor = 0; numeroFor <= 3 ; numeroFor = numeroFor + 1) {
+            System.out.println(numeroFor);
+        }
+    }
+}
